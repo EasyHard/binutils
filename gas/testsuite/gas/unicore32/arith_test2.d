8413908f5e83a8a3725a0110049cc8909987e7b9
LIU Zhiyou <liuzhiyou.cs@gmail.com>
2013-02-16 01:27:40 +0800

    Testcase for register alias name

---
+++ gas/testsuite/gas/unicore32/arith_test2.d
@@ -0,0 +1,14 @@
+#as:
+#objdump:   -dr
+#name:    arith_test2
+.*: +file format .*
+
+
+Disassembly of section .text:
+
+00000000 <_start>:
+   0:	0220c005 	xor r3, r4, r5
+   4:	02de801c 	xor sl, fp, ip
+   8:	02de801c 	xor sl, fp, ip
+   c:	02f7401f 	xor sp, lr, pc
+  10:	02f7401f 	xor sp, lr, pc
---
+++ gas/testsuite/gas/unicore32/arith_test2.s
@@ -0,0 +1,7 @@
+.global _start
+_start:
+        xor r3, r4, r5
+        xor sl, fp, ip
+        xor r26, r27, r28
+        xor sp, lr, pc
+        xor r29, r30, r31
